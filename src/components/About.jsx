ff18b84fee45a8fe78cf8b86cb6b2287c129afcc
ifemi27 <opatolaifeoluwav@gmail.com>
2023-04-01 00:22:18 -0700

    about page built

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import blue from '../assets/blue.png'
 
 const About = () => {
   return (
-    <div>
-        <div>
-            <h2>About Us</h2>
+    <div className='py-4 px-2'>
+        <div className='bg-cover h-[30vh] w-[55%] flex flex-col items-end justify-center container px-2'  style={{ backgroundImage: `url(${blue})` }}>
+        
         </div>
     </div>
   )
